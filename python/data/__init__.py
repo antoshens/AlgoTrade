ec6bc4c21b8dbe7a9a88c2e3bc2ea78668a58ce1
Anton <anthonygoncharenko@gmail.com>
2026-08-01 16:02:00 +0300

    Reorganize notebooks

---
+++ python/data/__init__.py
@@ -0,0 +1,3 @@
+from .download_data import download_tickers_history
+
+__all__ = ["download_tickers_history"]
---
+++ python/data/download_data.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from datetime import datetime
+import pandas as pd
+import yfinance as yf
+
+def download_tickers_history(start_date: datetime, end_date: datetime, tickers: list[str]) -> pd.DataFrame | None:   
+    """
+        Downloads the selected ticker price history data from Yahoo Finance engine.
+        
+        Returns
+        -------
+        Open : Price at market open
+        High : Highest price during the trading session
+        Low : Lowest price during the trading session
+        Close : Price at market close
+        Volume : Number of shares traded
+    """
+
+    # download market data for a list of tickers
+    tckrs_history = yf.download(
+        tickers=tickers,
+        start=start_date,
+        end=end_date,
+        interval="1d",
+        group_by="ticker",
+        auto_adjust=True,
+        progress=False,
+        threads=True,
+    );
+
+    return tckrs_history
